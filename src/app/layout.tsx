2261b28d2c0648f08583bc158944b7a716d2f0fc
Faruq Farkhan Iskandar <ruqfaruq30@gmail.com>
2025-06-12 14:25:35 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
+
 import type { Metadata } from 'next';
-import './globals.css';
+import Script from 'next/script';
 import { Header } from '@/components/layout/Header';
 import { Footer } from '@/components/layout/Footer';
 import { Toaster } from '@/components/ui/toaster';
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'eCommSim - Your Friendly Online Store',
@@ -14,20 +16,37 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const gtmId = 'GTM-KJDQC5M'; // Using the GTM ID from your original script
+
   return (
     <html lang="en">
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet" />
-        {/* START Google Analytics 4 */}
-        {/* END Google Analytics 4 */}
-        {/* START Google Tag Manager */}
-        {/* END Google Tag Manager */}
+        {/* Google Tag Manager - Head Script */}
+        <Script
+          id="gtm-head-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${gtmId}');
+            `,
+          }}
+        />
       </head>
       <body className="font-body antialiased flex flex-col min-h-screen">
-        {/* START Google Tag Manager (noscript) */}
-        {/* END Google Tag Manager (noscript) */}
+        {/* Google Tag Manager - Noscript Fallback */}
+        <noscript
+          dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${gtmId}"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+        />
         <Header />
         <main className="flex-grow container mx-auto px-4 py-8">
           {children}
