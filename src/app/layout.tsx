f362fb3defdcbe5f842ef04c4a213c316596351f
Faruq Farkhan Iskandar <ruqfaruq30@gmail.com>
2025-06-13 09:59:28 +0000

    Refactor GTM script in layout.tsx using next/script src attribute

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,11 +24,17 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet" />
-        {/* Google Tag Manager */}
+        {/* Google Tag Manager - Data Layer Initialization */}
+        <Script
+          id="dataLayer-init"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{ __html: `window.dataLayer = window.dataLayer || [];` }}
+        />
+        {/* Google Tag Manager - Main Script */}
         <Script
           id="gtm-script"
           strategy="beforeInteractive"
-          dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${gtmId}');` }}
+          src={`https://www.googletagmanager.com/gtm.js?id=${gtmId}`}
         />
       </head>
       <body className="font-body antialiased flex flex-col min-h-screen">
